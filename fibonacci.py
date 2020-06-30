776ea214e0f81c49aba82c00eac61d795a82aae3
Samdarshi Tiwari <30532659+samdarshihawk@users.noreply.github.com>
2020-06-30 18:55:22 -0400

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,23 @@
+"""
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, 
+such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0,   F(1) = 1
+F(N) = F(N - 1) + F(N - 2), for N > 1.
+
+Given N, calculate F(N).
+
+"""
+
+
+
+def fib(N)
+        a,b=0,1
+        if(N==0):
+            return(0)
+        elif (N==1):
+            return(1)
+        else:
+            for i in range(N):
+                b,a=b+a,b
+            return(a)
